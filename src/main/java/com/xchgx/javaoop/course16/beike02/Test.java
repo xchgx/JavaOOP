e0a4e86ef55da3d9d7957f84f91feda0764db685
xchgx <xchgx@163.com>
2020-06-01 21:31:06 +0800

    第16课 StringBuffer身份证号码识别 备课2 (检查是否符合身份证的规范)

---
+++ src/main/java/com/xchgx/javaoop/course16/beike02/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.xchgx.javaoop.course16.beike02;
+
+/**
+ *
+ * @author 陈老师 现代管理学院
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
