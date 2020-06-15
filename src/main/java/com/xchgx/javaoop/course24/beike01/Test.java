dd7d73d46cb9e6a7e30037bec6bb4c88274a2844
Administrator <Administrator@G4HIQX39NEMIOS5>
2020-06-15 16:37:29 +0800

    第24课 哔哩哔哩编程题-统计金闪闪-能召必召 测试

---
+++ src/main/java/com/xchgx/javaoop/course24/beike01/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xchgx.javaoop.course24.beike01;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Test {
+    public static void findedWuqi(int[] wuqis){
+        
+    }
+    public static void main(String[] args) {
+        //从数组中找出能够形成三角形的三把武器
+        //IPO思想
+        //输入: 一堆武器
+        //输出：能召唤出金闪闪的三把武器
+        //处理：搜索算法，找出符合条件的武器 findedWuqi
+        
+    }
+}
